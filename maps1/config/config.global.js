f43b7b3f707e6a7a88676d2d0666a54ca4c278d8
jlambert78 <janelambert@gmail.com>
2016-03-13 15:22:14 +1100

    asdf

--- maps1/config/config.global.js
+++ maps1/config/config.global.js
@@ -2,8 +2,8 @@ var config = module.exports = {};
  
 config.env = 'development';
 config.hostname = 'dev.example.com';
-config.mapprefix = 'http://10.1.1.9:3000'
-config.realm = 'http://10.1.1.9:3000/config/'
+config.mapprefix = 'http://10.1.1.4:3000'
+config.realm = 'http://10.1.1.4:3000/config/'
  
 //mongo database
 config.mongodb = {};
