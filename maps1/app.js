4096e4a9480a7311498a6c2647def31f09b2feae
jlambert78 <janelambert@gmail.com>
2016-03-13 15:26:28 +1100

    fx

--- maps1/app.js
+++ maps1/app.js
@@ -79,7 +79,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(qt.static(path.join(__dirname, 'public/uploadedimages/')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
