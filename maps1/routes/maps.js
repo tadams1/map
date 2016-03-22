99c060ed914510635730fdf6f8102c1e2f08fe9d
jlambert78 <janelambert@gmail.com>
2016-03-22 20:28:46 +1100

    CSV file download

--- maps1/routes/maps.js
+++ maps1/routes/maps.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var cfg = require('../config')
+var passwordless = require('passwordless');
 
 
 function getCSVData(val) {
@@ -31,12 +32,40 @@ router.get('/', function(req, res, next) {
       "features": []
     }
     
+   
+      obj1.features = docs;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(obj1);
+   });
+
+
+});
+
+/* GET users listing. */
+router.get('/data', passwordless.restricted(), function(req, res, next) {
+  var layers = req.app.get('layers');
+  var collection = layers.db.collection(cfg.mongodb.col);
+
+  var clause;
+  if(req.query.type == "ALL") {
+    clause = {};
+  }else if(req.query.type == "ALLTYPE") {
+    clause={"properties.type": req.query.types};
+  } else {
+    clause={"properties.typeid": req.query.type};
+  }
+  collection.find(clause,{'_id': 0}).toArray(function(e,docs){ 
+    var obj1 = {  
+      "type": "FeatureCollection",
+      "features": []
+    }
+    
     if(req.query.format == 'CSV') {
-      var s = "type,typeid,typetext,comment,icon,link,name,email,date,longitute,latitude\n";
+      var s = "type,typeid,typetext,comment,icon,link,firstname,surname,email,date,longitute,latitude\n";
       for(var i = 0; i<docs.length; i++){
         s += getCSVData(docs[i].properties.type) + ',' + getCSVData(docs[i].properties.typeid) + ',' + getCSVData(docs[i].properties.typetext) + ',';
         s += getCSVData(docs[i].properties.comment) + ','+ getCSVData(docs[i].properties.icon) + ',' + getCSVData(docs[i].properties.link) + ',';
-        s += getCSVData(docs[i].properties.yourname) + ',' + getCSVData(docs[i].properties.youremail)  + ',' + getCSVData(docs[i].properties.datesave) + ',';
+        s += getCSVData(docs[i].properties.firstname) + ',' + getCSVData(docs[i].properties.lastname) + ',' + getCSVData(docs[i].properties.youremail)  + ',' + getCSVData(docs[i].properties.datesaved) + ',';
         s += docs[i].geometry.coordinates[0] + ',' + docs[i].geometry.coordinates[1] + '\n';
       }
       res.contentType('csv');
@@ -52,5 +81,4 @@ router.get('/', function(req, res, next) {
 
 
 });
-
 module.exports = router;
